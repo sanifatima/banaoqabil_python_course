8b866a116d6e1ea137ea7b6a5e27581735ed884d
sanifatima <156574849+sanifatima@users.noreply.github.com>
2024-01-21 12:25:15 +0500

    Add files via upload

---
+++ amna_midterm.py
@@ -0,0 +1,46 @@
+#amna
+# Q1 add the two veriable and print thir
+num1 = int(input("Enter number:" ))
+num2 = int(input("Enter number: "))
+sum = num1 + num2
+print("the sum of " ,num1," + ",num2 ,"is",sum)
+      
+# Q2 
+ # division
+a = int(input("enter the number:"))
+b= int(input("enter the number:"))
+division = a / b
+print ("division: ",a,"/",b,"=",division) 
+
+# calcuating power
+power = num1 ** num2
+print(", power : ",num1, "^",num2, "is", power )
+
+#Q3
+c = int(input("enter temrature in celsius:"))
+f = (9/5 * c) +32
+print("37 degrees calsius is equal to ",f, "degrees fahrenheit")  
+
+#data type
+a = 10
+print(a)
+print(" the data type of ",a,"is", type(a))    
+
+b = 10.5
+print(b)
+print("the data tyoe of",b, "is,", type (b))
+
+d = True
+print (d) 
+print("the data type of",d,"is",type(d))
+
+e = "hello world"
+print(e)
+print("the vdata type of",e, "is" ,type(e))
+
+#list
+list = [1,2,3,4]
+print("list is:," list)
+print(" the data type of list is: ",type(list))
+
+        
